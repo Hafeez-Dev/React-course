134f111c2b6b4528ed5555cb36c96e98af9c3260
Abdul Hafeez <debugerxy@gmail.com>
2024-09-18 08:15:49 +0500

    add component to codebase

--- 01ViteReact/src/App.jsx
+++ 01ViteReact/src/App.jsx
@@ -1,7 +1,10 @@
+import {Component} from "./Component"
+
 function App() {
   return (
     <>
-      <h1>Vite + React series</h1>
+      <Component/>
+      <p>We learned how can I add a component to react application</p>
     </>
   )
 }
---
+++ 01ViteReact/src/Component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Component() {
+  return (
+    <>
+      <h3>vite + react series</h3>
+    </>
+  );
+}
+
+export {Component}; // named export with {..} for import we use {..} also
+// export default Component; // default export we can used both
