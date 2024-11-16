4aa876ccf7cff28cb2cd6405030ccacdb5e22858
Abdul Hafeez <debugerxy@gmail.com>
2024-11-15 21:05:39 -0800

    add one feature to codebase

--- 10TodoContextLocal/src/App.jsx
+++ 10TodoContextLocal/src/App.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoForm from "./components/TodoForm";
 import TodoItem from "./components/TodoItem";
+import { TodoProvider } from "./contexts/TodoContext";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    const id = Date.now();
+    setTodos((prevTodos) => [{ id, ...todo }, ...prevTodos]);
+  };
+
   return (
-    <div className="bg-[#172842] min-h-screen py-8">
-      <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-        <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-          Manage Your Todos
-        </h1>
-        <div className="mb-4">
-          <TodoForm />
-        </div>
-        <div className="flex flex-wrap gap-y-3">
-          <TodoItem />
+    <TodoProvider
+      value={{ todos, addTodo, deleteTodo, toggleComplete, updateTodo }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            <TodoItem />
+          </div>
         </div>
       </div>
-    </div>
+    </TodoProvider>
   );
 }
 
