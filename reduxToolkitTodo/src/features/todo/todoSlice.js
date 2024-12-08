d7631fed517f6c92257f136bd38de021d901c5f2
Abdul Hafeez <debugerxy@gmail.com>
2024-12-08 10:53:29 +0500

    RDT | Store Configured

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -7,5 +7,19 @@ const initialState = {
 const todoSlice = createSlice({
   name: "Todo",
   initialState,
-  reducers: {},
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
 });
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+export default todoSlice.reducer;
