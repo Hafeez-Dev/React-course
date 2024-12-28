7ea271e4aa2f65046157975342d5da4e3e7dd1bd
Abdul Hafeez <debugerxy@gmail.com>
2024-12-28 09:37:59 -0800

    Add Appwrite services for managing blog posts

---
+++ 12MegaBlog/src/appwrite/services.js
@@ -0,0 +1,85 @@
+import { Client, Databases, Storage, ID, Query } from "appwrite";
+import config from "../config/conf";
+
+class Services {
+    client = new Client();
+    database;
+    storage;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId)
+        this.database = new Databases(this.client);
+        this.storage = new Storage(this.client);
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.database.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: createPost :: error", error);
+        }
+    }
+
+    async updatePost( slug, { title, content, featuredImage, status}) {
+        try {
+            return await this.database.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: updatePost :: error", error);
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.database.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite serive :: deletePost :: error", error);
+            return false;
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            await this.database.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite serive :: getPost :: error", error);
+            return false;
+        }
+    }
+}
+
+
+const service = new Services();
+export default service;
