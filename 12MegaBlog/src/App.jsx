159f849e8f0af91474b678e507dd720f4c8b121d
Abdul Hafeez <debugerxy@gmail.com>
2024-12-31 10:19:44 -0800

    Integrate Tailwind CSS and Redux for enhanced styling and state management

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { login, logout } from './store/authSlice'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
 
 function App() {
-  return (
-    <div>
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((user) => {
+        if (user) {
+          dispatch(login({ userData: user }))
+        } else {
+          dispatch(logout())
+        }
+      }).finally(() => {
+        setLoading(false)
+      })
+  }, [dispatch])
+
+  return !loading ? (
+    <div className='flex justify-center items-center h-screen'>
       <h1>Final Mega Blogs Poroject</h1>
     </div>
-  )
+  ) : ("Loading...")
 }
 
 export default App
