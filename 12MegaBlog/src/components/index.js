43db63cbafa2f5b50bc2c562b37e9cc473002122
Abdul Hafeez <debugerxy@gmail.com>
2025-01-07 21:17:26 -0800

    Add Select component export to index.js

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,11 +3,13 @@ import Footer from './Footer/Footer'
 import Button from './Button'
 import LogoutBtn from './Header/LogoutBtn'
 import Input from './Input'
+import Select from './Select'
 
 export {
     Header,
     Footer,
     Button,
     LogoutBtn,
-    Input
+    Input,
+    Select,
 }
