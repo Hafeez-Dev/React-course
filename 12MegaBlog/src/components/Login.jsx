f5995ea180e7f48d24c4307a5e262c98a0db5994
Abdul Hafeez <debugerxy@gmail.com>
2025-01-18 07:55:57 -0800

    Add Login component for user authentication; set up initial structure and imports

---
+++ 12MegaBlog/src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React,{ useState } from 'react'
+import authService from '../appwrite/auth'
+import { Button, Input } from './index'
+import { Link, useNavigate } from 'react-router-dom'
+import { login as loginAction } from '../store/authSlice'
+import { useDispatch } from 'react-redux'
+import { useForm } from 'react-hook-form'
+
+function Login() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
