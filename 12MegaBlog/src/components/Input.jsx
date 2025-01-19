98031ef2d89cf5e75a4bf95d8d2f8d90568c4adf
Abdul Hafeez <debugerxy@gmail.com>
2025-01-19 10:06:53 -0800

    Refactor Input component and integrate it into Login form for improved structure and readability

--- 12MegaBlog/src/components/Input.jsx
+++ 12MegaBlog/src/components/Input.jsx
@@ -5,26 +5,22 @@ const Input = React.forwardRef(
     const id = useId();
     return (
       <>
-        <div className="bg-white p-4 rounded-lg">
-          <div className="relative bg-inherit">
-            <input
-              type={type}
-              id={id}
-              ref={ref}
-              placeholder={label}
-              className={`${className} peer bg-transparent h-10 w-72 rounded-lg text-gray-900 placeholder-transparent ring-2 px-2 ring-gray-500 focus:ring-sky-600 focus:outline-none focus:border-rose-600`}
-              {...props}
-            />
-            {label && (
-              <label
-                htmlFor={id}
-                className={`absolute cursor-text left-0 -top-3 text-sm text-gray-500 bg-inherit mx-1 px-1 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm transition-all`}
-              >
-                {label}
-              </label>
-            )}
-          </div>
-        </div>
+        <input
+          type={type}
+          id={id}
+          ref={ref}
+          placeholder={label}
+          className={`${className} peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600`}
+          {...props}
+        />
+        {label && (
+          <label
+            htmlFor={id}
+            className={`absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm`}
+          >
+            {label}
+          </label>
+        )}
       </>
     );
   }
--- 12MegaBlog/src/components/Login.jsx
+++ 12MegaBlog/src/components/Login.jsx
@@ -38,43 +38,30 @@ function Login() {
               <h1 className="text-2xl font-semibold">Login</h1>
             </div>
             <div className="divide-y divide-gray-200">
-            {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-              <form>
+              {error && (
+                <p className="text-red-600 mt-8 text-center">{error}</p>
+              )}
+              <form onSubmit={handleSubmit(login)}>
                 <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                   <div className="relative">
-                    <input
-                      autoComplete="off"
-                      id="email"
-                      name="email"
-                      type="text"
-                      className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                      placeholder="Email address"
+                    <Input
+                      label="Email Adress"
+                      type="email"
+                      {...register("email")}
                     />
-                    <label
-                      htmlFor="email"
-                      className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                    >
-                      Email Address
-                    </label>
                   </div>
                   <div className="relative">
-                    <input
-                      autoComplete="off"
-                      id="password"
-                      name="password"
+                    <Input
+                      label="Password"
                       type="password"
-                      className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                      placeholder="Password"
+                      {...register("password")}
                     />
-                    <label
-                      htmlFor="password"
-                      className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                    >
-                      Password
-                    </label>
                   </div>
                   <div className="relative">
-                    <Button className="bg-cyan-500 text-white rounded-md px-2 py-1">
+                    <Button
+                      className="bg-cyan-500 text-white rounded-md px-2 py-1"
+                      type="submit"
+                    >
                       Submit
                     </Button>
                   </div>
