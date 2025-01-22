9e449ffeb175d89936a1d77c9990933eafe8ec23
Abdul Hafeez <debugerxy@gmail.com>
2025-01-22 09:22:21 -0800

    Add Signup component with form handling and validation

--- 12MegaBlog/src/components/Signup.jsx
+++ 12MegaBlog/src/components/Signup.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
+import React, { useState } from "react";
+import authService from "../appwrite/auth";
+import { Button, Input } from "./index";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../store/authSlice";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
 
 function Signup() {
+  const { register, handleSubmit } = useForm();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const signup = async (data) => {
+    try {
+      setError("");
+      const signingUp = await authService.signup(data);
+      if (signingUp) {
+        const userData = await authService.getCurrentUser();
+        if (userData) {
+          dispatch(login({ userData }));
+          navigate("/");
+        }
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <div>
-      
+      <div>
+        <h1>Signup</h1>
+      </div>
+      <div>
+        {error && <p>{error}</p>}
+        <form onSubmit={handleSubmit(signup)}>
+          <div>
+            <Input
+              label="Email Adress"
+              type="email"
+              {...register("email", {
+                required: true,
+                validate: {
+                  matchPattrn: (value) =>
+                    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                    "Email address must be a valid address",
+                },
+              })}
+            />
+          </div>
+          <div>
+            <Input
+              label="Password"
+              type="password"
+              {...register("password", {
+                required: true,
+                minLength: 6,
+              })}
+            />
+          </div>
+          <div>
+            <Button type="submit">Signup</Button>
+          </div>
+        </form>
+        <div>
+          <Link to="/login">Login</Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -6,6 +6,7 @@ import Input from './Input'
 import Select from './Select'
 import PostCard from './PostCard'
 import Login from './Login'
+import Signup from './Signup'
 
 export {
     Header,
@@ -15,5 +16,6 @@ export {
     Input,
     Select,
     PostCard,
-    Login
+    Login,
+    Signup
 }
