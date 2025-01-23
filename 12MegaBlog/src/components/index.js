c2977525339a13093ff2ac07e51516b0112728b4
Abdul Hafeez <debugerxy@gmail.com>
2025-01-23 09:48:04 -0800

    Format imports in index.js for improved readability

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,21 +1,21 @@
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
-import Button from './Button'
-import LogoutBtn from './Header/LogoutBtn'
-import Input from './Input'
-import Select from './Select'
-import PostCard from './PostCard'
-import Login from './Login'
-import Signup from './Signup'
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Button from "./Button";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Select from "./Select";
+import PostCard from "./PostCard";
+import Login from "./Login";
+import Signup from "./Signup";
 
 export {
-    Header,
-    Footer,
-    Button,
-    LogoutBtn,
-    Input,
-    Select,
-    PostCard,
-    Login,
-    Signup
-}
+  Header,
+  Footer,
+  Button,
+  LogoutBtn,
+  Input,
+  Select,
+  PostCard,
+  Login,
+  Signup,
+};
